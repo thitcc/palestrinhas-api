53bd4793f01dfae8a2416c9f83168ba17d09ccdf
Thiago Tenório <thitccp@gmail.com>
2023-10-19 21:39:17 -0300

    feat: add request spec helper This file contains useful methods to use in specs

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -9,6 +9,7 @@ require 'rspec/rails'
 require 'support/factory_bot'
 require 'support/database_cleaner_active_record'
 require 'support/shoulda_matchers'
+require 'support/request_spec_helper'
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
